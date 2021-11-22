775d17b2161ab74c75db07cbcda1d221f6763c55
meet-aniket <meet.aniket.kumar@gmail.com>
2021-11-22 18:42:36 +0530

    Some updates are done

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <NavBar />
+    <div>
+      <button
+        v-for="language in languages"
+        :key="language.title"
+        @click="changeLocale(language.lang)"
+      >
+        <flag :iso="language.flag" v-bind:squared="false" />
+        {{ language.title }}
+      </button>
+    </div>
+    <router-view />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import NavBar from "./layouts/NavBar.vue";
+import Footer from "./layouts/Footer.vue";
+import i18n from "@/plugins/i18n";
+
+export default {
+  name: "App",
+  components: { NavBar, Footer },
+  data() {
+    return {
+      languages: [
+        { flag: "us", lang: "en", title: "English" },
+        { flag: "in", lang: "hi", title: "Hindi" },
+      ],
+    };
+  },
+  methods: {
+    changeLocale(locale) {
+      i18n.setLocale(locale);
+    },
+  },
+};
+</script>
+<style scoped>
+button {
+  padding: 5px;
+  border: 1px solid green;
+  font-size: 10px;
+  margin: 10px;
+}
+</style>
---
+++ src/views/pageNotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <center class="center-block mt-5">
+    <h3 class="error">{{ $t("error") }}</h3>
+    <h3 class="lead error">{{ $t("pageNotFound") }}</h3>
+  </center>
+</template>
+
+<script>
+export default {
+  name: "PageNotFound",
+};
+</script>
+
+<style>
+.error {
+  font-weight: bold;
+}
+</style>

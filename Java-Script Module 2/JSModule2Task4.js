7bb88c455c702fd4e9417d7808ca088bd9458ee8
Mayank Verma <vermayank909@gmail.com>
2022-03-01 18:26:39 +0530

    Task 4 Make every elements half and twice of the list is completed.

---
+++ Java-Script Module 2/JSModule2Task4.js
@@ -0,0 +1,16 @@
+// Provided the following  array:
+// [0, 2, 4, 6, 8, 10, 12, 14, 16]
+var list = [0, 2, 4, 6, 8, 10, 12, 14, 16];
+// Generate and print another list such that: 
+// a. every element being twice of elements in the current list.
+var current1 = list.map((ele) => {
+    return ele * 2;
+});
+console.log("every element being twice of elements in the current list.");
+console.log(current1);
+// b. every element being half of the elements in the current list.
+var current2 = list.map((ele) => {
+    return ele / 2;
+});
+console.log("every element being half of elements in the current list.");
+console.log(current2);

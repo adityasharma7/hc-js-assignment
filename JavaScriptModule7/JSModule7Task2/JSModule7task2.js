7e9e9ef9710dc3baa1f345e56980a647aaa8b3bd
Mayank Verma <vermayank909@gmail.com>
2022-03-24 15:07:07 +0530

    JSModule7 Task2 Completed: change color of cell when click revert if again click

---
+++ JavaScriptModule7/JSModule7Task2/JSModule7task2.js
@@ -0,0 +1,12 @@
+let cell = document.querySelectorAll("tr>th,td");
+for (let i = 0; i < cell.length; i++) {
+    cell[i].addEventListener("click", (e) => {
+        if (!(cell[i].style.backgroundColor == "red")) {
+            cell[i].style.backgroundColor = 'red';
+        }
+        else {
+            cell[i].style.backgroundColor = "";
+        }
+    });
+}
+

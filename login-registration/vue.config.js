dbe61a878500293776582c246484c0b423e12834
Mayank Verma <vermayank909@gmail.com>
2022-04-19 19:12:38 +0530

    VueJS Module 6 All Tasks are Completed.

--- login-registration/vue.config.js
+++ login-registration/vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  pluginOptions: {
+    i18n: {
+      locale: 'y',
+      fallbackLocale: 'y',
+      localeDir: 'y',
+      enableLegacy: true,
+      runtimeOnly: false,
+      compositionOnly: true,
+      fullInstall: true
+    }
+  }
 })

ea849f49f101a986da34edd1c15713dc66c31eb6
Mayank Verma <vermayank909@gmail.com>
2022-04-25 11:38:37 +0530

    Ionic Training Task Completed: Build Netflix/Twitter UI using Ionic components.

---
+++ Netflix-UI-Ionic/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <ion-app>
+    <ion-router-outlet />
+  </ion-app>
+</template>
+
+<script lang="ts">
+import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    IonApp,
+    IonRouterOutlet
+  }
+});
+</script>
+<style >
+:root{
+  --ion-background-color: #000;
+  --ion-text-color: #fff;
+}
+</style>

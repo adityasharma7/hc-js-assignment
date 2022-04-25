4d7f10b4074ce22aa2a0a8b51a37ad2262403766
niharika <niharika2nas@gmail.com>
2022-04-25 20:21:29 +0530

    Implemented home page module-4 task 2

--- e-commerce/src/views/HomeView.vue
+++ e-commerce/src/views/HomeView.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+   <Header/>
+    <!-- <HelloWorld/> -->
+    <Categories/>
+    <Featuredproduct/>
+    <BannerHero/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import HelloWorld from '@/components/HelloWorld'
+//import App from '@/views/App.vue'
+import BannerHero from '@/components/BannerHero'
+import Categories from '@/components/Categories'
+import Featuredproduct from '@/components/Featuredproduct'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    // HelloWorld,
+    //App,
+    BannerHero,
+    Categories,
+    Featuredproduct,
+    Header,
+    Footer
   }
 }
 </script>

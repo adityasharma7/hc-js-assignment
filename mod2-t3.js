dc01887244c983b981afa70e305bc3aa17e623df
rakeshrajput999 <baajigarrakesh@gmail.com>
2022-05-20 18:28:52 +0530

    mod2-t3.js completed

--- mod2-t3.js
+++ mod2-t3.js
@@ -1,31 +1,48 @@
 var a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// even
+console.log(a);
+var x = a.filter((item) => item % 2 == 0);
 
-// var x= a.filter((item)=>item%2==0)
+console.log("even value");
 
-// console.log(x)
+console.log(x);
 
-// var x= a.filter((item)=>item%2!==0)
+var x = a.filter((item) => item % 2 !== 0);
 
-// console.log(x)
+console.log("odd values");
+console.log(x);
 
 var x = a.filter((item) => {
   let c = 0;
-  for (let i = 1; i <= item; i++) {
-    if (item % i !== 0) {
+ for (let i = 0; i <= item; i++) {
+    if (item % i == 0) {
       c++;
     }
   }
-  if (item <= 2) {
+  if (c <= 2) {
     return true;
   } else {
-    if (c > 2) {
-      return true;
-    } else {
-      return false;
-    }
+    return false;
   }
 });
 
+console.log("prime values");
 console.log(x);
+
+function isprime(n) {
+  let count = 0;
+  for (let i = 0; i <= n; i++) {
+    if (n % i == 0) {
+      count = count + 1;
+    }
+  }
+  if (count <= 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var y = a.filter(isprime);
+console.log("second approach");
+console.log(y);

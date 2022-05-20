e89c5e6cc6b2701cf1a1630cf9cc53e59a1a5b49
rakeshrajput999 <baajigarrakesh@gmail.com>
2022-05-20 13:14:52 +0530

    t5-d completed

---
+++ t5-d.js
@@ -0,0 +1,16 @@
+var arr=    [10, 1, 42, 36, 4, 75, 6, 97, 81, 19, 10]
+
+
+var removeddublicates  = Array(...new Set(arr))
+
+console.log( removeddublicates)
+ var x=Object.keys(arr)
+ console.log(x)
+
+
+ var x=Object.values(arr)
+ console.log(x)
+
+
+ var x=Object.entries(arr)
+ console.log(x)

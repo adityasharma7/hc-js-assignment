520fa1feff8cf28fe49efff461b5167443ae532c
rakeshrajput999 <baajigarrakesh@gmail.com>
2022-05-24 19:38:52 +0530

    avarage question

--- mod3-t1.js
+++ mod3-t1.js
@@ -12,3 +12,33 @@ const min = Obj.reduce((acc, ele) => {
 let { id, name, age } = Obj.find((ele) => ele.age === min);
 
 console.log(" the younest one is " + name + " having id " + id);
+// answer b
+
+
+
+
+
+// c avarage age
+
+const sum = Obj.reduce((acc, ele) => {
+  acc = acc + ele.age;
+  return acc;
+}, 0);
+
+console.log("the avarage age is : " + sum / Obj.length);
+
+
+
+// ans wer d
+let R = [];
+let A = [];
+for (ele of Obj) {
+  if (ele.name[0] === "R") {
+    R.push(ele.name);
+  }
+  if (ele.name[0] === "A") {
+    A.push(ele.name);
+  }
+}
+
+console.log(R, A);

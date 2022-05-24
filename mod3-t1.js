37a1225aaa0838c31c0a64d1941b3b11e96de5ee
rakeshrajput999 <baajigarrakesh@gmail.com>
2022-05-24 16:44:06 +0530

    ...

---
+++ mod3-t1.js
@@ -0,0 +1,9 @@
+const Obj = [
+  { id: 1, name: "Amit Kumar", age: 25 },
+  { id: 2, name: "Rahul Dixit", age: 20 },
+  { id: 3, name: "Ravi Joshi", age: 55 },
+  { id: 4, name: "Rohit Verma", age: 35 },
+  { id: 5, name: "Ajay Jain", age: 17 },
+];
+let minAge = Infinity;
+let maxindex = Obj.findIndex((value, index) => {})

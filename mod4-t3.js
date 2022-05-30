68b2f3dc005afb249ec6b8af3217504d0316050f
rakeshrajput999 <baajigarrakesh@gmail.com>
2022-05-30 10:02:07 +0530

    t3completed

--- mod4-t3.js
+++ mod4-t3.js
@@ -1,3 +1,15 @@
-function sum(a,b){
-    a+b
+function sum(...nums) {
+  if (nums.length === 0) {
+    return 0;
+  }
+  function add(nums, i) {
+    if (i === nums.length - 1) {
+      return nums[i];
+    }
+    return nums[i] + add(nums, i + 1);
+  }
+  return add(nums, 0);
 }
+
+var x = sum(1,2,3,4,6,7,8);
+console.log(x);

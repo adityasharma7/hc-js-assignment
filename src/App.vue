20be82a2605a29b2926f0186190b44cead6e93e2
rakeshrajput999 <baajigarrakesh@gmail.com>
2022-06-27 14:53:55 +0530

    created basic structure of login and signup page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+   <button @click="tab='Login'">Login</button>
+   <button @click="tab='SignUp'">SignUp</button>
+   <component :is="tab"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/Login.vue'
+import SignUp from './components/SignUp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Login,
+    SignUp
+    
+  },
+  data(){
+    return{
+      tab:'Login'
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>

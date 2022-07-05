1ae5733c97a7146c581605bc69a64f4098af1d36
rakeshrajput999 <baajigarrakesh@gmail.com>
2022-07-05 16:04:44 +0530

    product filter ui implementation

---
+++ flipKart-clone/src/components/ProductFilter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="sidenav">
+  <a href="#">Link</a>
+  <a href="#">Link</a>
+  <a href="#">Link</a>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.sidenav {
+  height: 100%;
+  width: 300px;
+  position: absolute;
+  margin: 30px;
+  left: 0;
+  background-color: #111;
+  overflow-x: hidden;
+  margin: 30px;
+}
+</style>
--- flipKart-clone/src/views/CategoryListing.vue
+++ flipKart-clone/src/views/CategoryListing.vue
@@ -1,6 +1,7 @@
 <template>
 <div >
     <!-- hello from {{$route.params.category}} -->
+    <ProductFilter/>
     <div class="grid-container" v-for="ele in $store.state.Storedata" :key="ele.id">
          <Card class="grid-item" v-if="ele.category===$route.params.category" :image="ele.image" :title="ele.title" :price="ele.price" :rating="ele.rating.rate" />
     </div>
@@ -9,9 +10,10 @@
 
 <script>
 import Card from '../components/Card.vue'
+import ProductFilter from '../components/ProductFilter.vue'
 export default {
   name:"CateogoryListing",
-  components: {Card},
+  components: { Card, ProductFilter },
   data() {
     return {
 
@@ -21,18 +23,20 @@ export default {
 </script>
 
 <style scoped>
+
 .grid-container {
   display: grid;
   grid-template-rows: 1fr,1fr,1fr;
-  padding: 10px;
+  margin-left: 300px;
+  padding-left: 20px;
   gap: 50px;
 }
 .grid-item {
   background-color: rgba(255, 255, 255, 0.8);
   border: 1px solid rgba(0, 0, 0, 0.8);
   padding: 20px;
-  width: 20vw;
-  height: 40vh;
+  width: 70vw;
+  height: 35vh;
   font-size: 30px;
   text-align: center;
 }

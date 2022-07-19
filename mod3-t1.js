88de50b3e4a7052b24f79eb1bfee177f7a7b5fd4
rakeshrajput999 <baajigarrakesh@gmail.com>
2022-07-19 16:23:05 +0530

    implemeted Math functions

--- mod3-t1.js
+++ mod3-t1.js
@@ -1,3 +1,4 @@
+
 const Obj = [
   { id: 1, name: "Amit Kumar", age: 25 },
   { id: 2, name: "Rahul Dixit", age: 20 },
@@ -5,13 +6,24 @@ const Obj = [
   { id: 4, name: "Rohit Verma", age: 35 },
   { id: 5, name: "Ajay Jain", age: 17 },
 ];
-const min = Obj.reduce((acc, ele) => {
-  return acc.age < ele.age ? acc.age : ele.age;
-}, Infinity);
 
-let { id, name, age } = Obj.find((ele) => ele.age === min);
+//  Print id and name of the youngest and oldest person
+ const ages = Obj.reduce((acc,ele)=>{
+  return acc.concat(ele.age)
+  },[])
+  const min = Math.min(...ages) 
+  const max = Math.max(...ages)
 
-console.log(" the younest one is " + name + " having id " + id);
+  var youngest= Obj.find((ele) => ele.age === min)
+  console.log(" the younest one is " + youngest.name + " having id " +youngest.id+ "having age " + youngest.age+ ".")
+
+
+  var oldest = Obj.find((ele) => ele.age === max)
+  console.log(" the oldest one is " + oldest.name + " having id " +oldest.id + "having age " +oldest.age+ ".")
+
+
+
+
 // answer b
 
 

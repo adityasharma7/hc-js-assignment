7765fa51279036ff6a2442840fd6b649e0e78bc0
Kanchan <kanchan2001pal@gmail.com>
2022-08-22 13:22:34 +0530

    Completed Vue Module 2 Task

---
+++ VueTask2/VueTask2/src/App.vue
@@ -0,0 +1,41 @@
+
+
+
+<script>
+
+import Login from '/home/kanchan/Learning/HotwaxTraining/hc-js-assignment/VueTask2/VueTask2/src/components/Login.vue'
+import SignUp from '/home/kanchan/Learning/HotwaxTraining/hc-js-assignment/VueTask2/VueTask2/src/components/SignUp.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Login,
+    SignUp,
+    Login
+},
+
+
+
+  }
+
+</script>
+
+
+ <template>
+  <div>
+    <Login />
+    <SignUp />
+
+
+
+ 
+  </div>
+
+
+
+</template>
+
+
+<style>
+</style>

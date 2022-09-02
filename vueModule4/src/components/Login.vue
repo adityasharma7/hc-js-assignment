ed6de86ccb4ea012a4fce4a6a6a8ac1c23a4e0d7
Kanchan <kanchan2001pal@gmail.com>
2022-09-02 15:55:00 +0530

    Store the token in local storage

--- vueModule4/src/components/Login.vue
+++ vueModule4/src/components/Login.vue
@@ -1,43 +1,75 @@
 <template>
+  <div>
+    <h1 style="text-align: center; margin-top: 40px">Login Page</h1>
 
+    <form>
+      <label for="alreadyRe">Already Have An Account ?</label> <br />
+      <fieldset>
+        <legend style="font-weight: bold"><h2>SignIn</h2></legend>
 
-    <div>
-  
-  
-  
-     <h1 style="text-align: center; margin-top: 40px;">Login Page</h1>
-  
-  
-  
-      <form >
-          <label for="alreadyRe">Already Have An Account ?</label> <br>
-      <fieldset> 
-          <legend style="font-weight: bold;"><h2>SignIn</h2></legend>
-  
-          <label for="Username">Enter UserName</label> <br>
-          <input type="text" id="Username" name="Username" size="30"><br><br>
-  
-          <label for="password">Enter Password</label> <br>
-          <input type="password" id="password" name="password" size="30"><br><br>
-  
+        <label for="Username">Enter Username</label> <br />
+        <input
+          type="text"
+          id="email"
+          name="email"
+          size="30"
+          v-model="username"/> <br /> <br />
+
+        <label for="password">Enter Password</label> <br />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          size="30"
+          v-model="password"
+        /><br /><br />
+        <button type="button" class="btn btn-primary" @click="loginUser()">Login</button>
       </fieldset>
-  </form>
+    </form>
+  </div>
+</template>
   
-  
-  
-    </div>
-  
-  
-  </template>
-  
-  <script>
-  export default {
-      name: 'Login'
-  }
-  </script>
+ 
+
+<script>
+import axios from "axios";
+export default {
+  name: "Login",
+
+  data() {
+    return {
+      username: "mor_2314",
+      password: "83r5^_",
+    };
+  },
+
+  methods: {
+          async loginUser() {
+
+
+        const cardentials  = {
+            username: this.username,
+            password: this.password
+        }
+        
+        const response = await axios.post('https://fakestoreapi.com/auth/login', cardentials)
+         localStorage.setItem('token' , response.data.token)
+//    this.$router.push('/') 
+
+
+    }
+  },
+}
+
+</script>
+
+
+
   
   <style>
-  form {
-  text-align: center; margin-top: 40px; background-color: azure;
-  }
-  </style>
+form {
+  text-align: center;
+  margin-top: 40px;
+  background-color: azure;
+}
+</style>
